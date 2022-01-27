59f86403bf881eeac2efd3e6efdbc323ccda36fe
Shadi Naif <shadinaif@gmail.com>
2022-01-27 18:43:20 +0200

    Create helpers

---
+++ tahoe_sites/tests/test_helpers.py
@@ -0,0 +1,192 @@
+"""
+Tests for models
+"""
+import pytest
+from django.conf import settings
+from organizations.models import Organization
+
+from tahoe_sites import helpers
+from tahoe_sites.models import TahoeSiteUUID
+from tahoe_sites.tests.fatories import UserFactory
+from tahoe_sites.tests.test_models import DefaultsForTestsMixin
+from tahoe_sites.tests.utils import create_organization_mapping
+
+
+class TestHelpers(DefaultsForTestsMixin):
+    """
+    Tests for helpers
+    """
+    def setUp(self):
+        super().setUp()
+        self.org1 = None
+        self.org2 = None
+        self.mapping = None
+        self.user2 = None
+
+    def _prepare_mapping_data(self):
+        """
+        mapping:
+            default_org --> default_user
+            Org1        --> default_user  -----> self.mapping points here
+            Org1        --> user2
+            Org2        --> user2
+            Org3        --> None
+        """
+        self.org1 = self._create_organization(name='Org1', short_name='O1')
+        create_organization_mapping(user=self.default_user, organization=self.default_org)
+        self.mapping = create_organization_mapping(user=self.default_user, organization=self.org1)
+
+        self.org2 = self._create_organization(name='Org2', short_name='O2')
+        self.user2 = UserFactory.create()
+        create_organization_mapping(user=self.user2, organization=self.org1)
+        create_organization_mapping(user=self.user2, organization=self.org2)
+
+        self._create_organization(name='Org3', short_name='O3')
+
+        # We have four organizations
+        assert Organization.objects.count() == 4
+
+    @pytest.mark.skipif(settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
+                        reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is off')
+    def test_get_organization_by_uuid_without_org(self):
+        """
+        Test get_organization_by_uuid helper when edx-organizations customization is off
+        """
+        default_site = TahoeSiteUUID.objects.get(organization=self.default_org)
+        assert helpers.get_organization_by_uuid(default_site.site_uuid) == self.default_org
+
+    @pytest.mark.skipif(settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
+                        reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is off')
+    def test_get_uuid_by_organization_without_org(self):
+        """
+        Test get_uuid_by_organization helper when edx-organizations customization is off
+        """
+        default_site = TahoeSiteUUID.objects.get(organization=self.default_org)
+        assert helpers.get_uuid_by_organization(self.default_org) == default_site.site_uuid
+
+    @pytest.mark.skipif(not settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
+                        reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is on')
+    def test_get_organization_by_uuid_with_org(self):
+        """
+        Test get_organization_by_uuid helper when edx-organizations customization is on
+        """
+        assert helpers.get_organization_by_uuid(self.default_org.edx_uuid) == self.default_org
+
+    @pytest.mark.skipif(not settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
+                        reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is on')
+    def test_get_uuid_by_organization_with_org(self):
+        """
+        Test get_uuid_by_organization helper when edx-organizations customization is on
+        """
+        assert helpers.get_uuid_by_organization(self.default_org) == self.default_org.edx_uuid
+
+    def test_get_organizations_for_user_default(self):
+        """
+        Verify that get_active_organizations_for_user helper returns only related to active user
+        """
+        self._prepare_mapping_data()
+
+        # default_user is mapped to 2 of them
+        assert list(helpers.get_organizations_for_user(self.default_user)) == [self.default_org, self.org1]
+
+        # user2 is mapped to two of them, one shared with default_user
+        assert list(helpers.get_organizations_for_user(self.user2)) == [self.org1, self.org2]
+
+        # records with inactive user will not be returned
+        self.mapping.is_active = False
+        self.mapping.save()
+        assert list(helpers.get_organizations_for_user(self.default_user)) == [self.default_org]
+
+    def test_get_organizations_for_user_with_inactive_users(self):
+        """
+        Verify that get_active_organizations_for_user helper can return all organization related to a user
+        including organizations having that user deactivated
+        """
+        self._prepare_mapping_data()
+
+        self.mapping.is_active = False
+        self.mapping.save()
+        assert list(helpers.get_organizations_for_user(self.default_user, with_inactive_users=True)) == [
+            self.default_org,
+            self.org1
+        ]
+
+    def test_get_organizations_for_user_without_admins(self):
+        """
+        Verify that get_active_organizations_for_user helper can return all organization related to a user
+        excluding organizations having that user as an admin
+        """
+        self._prepare_mapping_data()
+
+        self.mapping.is_admin = True
+        self.mapping.save()
+        assert list(helpers.get_organizations_for_user(self.default_user, without_admins=True)) == [self.default_org]
+
+    def test_get_users_of_organization(self):
+        """
+        Verify that get_users_of_organization returns all active users related to an organization
+        """
+        self._prepare_mapping_data()
+
+        # default_org is mapped to default_user
+        assert list(helpers.get_users_of_organization(self.default_org)) == [self.default_user]
+
+        # Org1 is mapped to two users
+        assert list(helpers.get_users_of_organization(self.org1)) == [self.default_user, self.user2]
+
+        # inactive users will not be returned
+        self.mapping.is_active = False
+        self.mapping.save()
+        assert list(helpers.get_users_of_organization(self.org1)) == [self.user2]
+
+    def test_get_users_of_organization_with_inactive_users(self):
+        """
+        Verify that get_users_of_organization helper can return all user related to an organization
+        including deactivated users
+        """
+        self._prepare_mapping_data()
+
+        self.mapping.is_active = False
+        self.mapping.save()
+        assert list(helpers.get_users_of_organization(self.org1, with_inactive_users=True)) == [
+            self.default_user,
+            self.user2
+        ]
+
+    def test_get_users_of_organization_without_admins(self):
+        """
+        Verify that get_users_of_organization helper can return all user related to an organization
+        excluding admin users
+        """
+        self._prepare_mapping_data()
+
+        self.mapping.is_admin = True
+        self.mapping.save()
+        assert list(helpers.get_users_of_organization(self.org1, without_admins=True)) == [self.user2]
+
+    def test_is_active_admin_on_any_organization(self):
+        """
+        Verify that is_active_admin_on_any_organization helper returns True if the given user
+        is an admin on any of the given organizations
+        """
+        self._prepare_mapping_data()
+        org_ids = Organization.objects.values('id')
+
+        assert not helpers.is_active_admin_on_any_organization(user=self.default_user, org_ids=org_ids)
+
+        self.mapping.is_admin = True
+        self.mapping.save()
+        assert helpers.is_active_admin_on_any_organization(user=self.default_user, org_ids=org_ids)
+
+    def test_is_active_admin_on_organization(self):
+        """
+        Verify that is_active_admin_on_organization helper returns True if the given user
+        is an admin on the given organization
+        """
+        self._prepare_mapping_data()
+
+        assert not helpers.is_active_admin_on_organization(user=self.default_user, organization=self.org1)
+
+        self.mapping.is_admin = True
+        self.mapping.save()
+        assert helpers.is_active_admin_on_organization(user=self.default_user, organization=self.org1)
--- tahoe_sites/tests/test_models.py
+++ tahoe_sites/tests/test_models.py
@@ -9,6 +9,7 @@ from organizations.models import Organization
 
 from tahoe_sites.models import TahoeSiteUUID, UserOrganizationMapping
 from tahoe_sites.tests.fatories import UserFactory
+from tahoe_sites.tests.utils import create_organization_mapping
 from tahoe_sites.zd_helpers import should_site_use_org_models
 
 
@@ -16,15 +17,21 @@ class DefaultsForTestsMixin(TestCase):
     """
     Mixin that creates some default objects
     """
+    def _create_organization(self, name, short_name, active=True):  # pylint: disable=no-self-use
+        return Organization.objects.create(
+            name=name,
+            description='{name} description'.format(name=name),
+            active=active,
+            short_name=short_name,
+        )
+
     def setUp(self) -> None:
         """
         Initialization
         """
-        self.default_org = Organization.objects.create(
+        self.default_org = self._create_organization(
             name='test organization',
-            description='test organization description',
-            active=True,
-            short_name='TO',
+            short_name='TO'
         )
         self.default_user = UserFactory.create()
 
@@ -38,10 +45,9 @@ class TestUserOrganizationMapping(DefaultsForTestsMixin):
         """
         Having the same user for the same organization should not be allowed
         """
-        UserOrganizationMapping.objects.create(
+        create_organization_mapping(
             user=self.default_user,
             organization=self.default_org,
-            is_admin=True,
         )
         assert UserOrganizationMapping.objects.count() == 1
 
@@ -49,20 +55,18 @@ class TestUserOrganizationMapping(DefaultsForTestsMixin):
             expected_exception=IntegrityError,
             expected_message='UNIQUE constraint failed: tahoe_sites_userorganizationmapping.user_id,'
         ):
-            UserOrganizationMapping.objects.create(
+            create_organization_mapping(
                 user=self.default_user,
                 organization=self.default_org,
-                is_admin=True,
             )
 
     def test_to_string(self):
         """
         Verify format of auto convert to string
         """
-        mapping = UserOrganizationMapping.objects.create(
+        mapping = create_organization_mapping(
             user=self.default_user,
             organization=self.default_org,
-            is_admin=True,
         )
         assert str(mapping) == 'UserOrganizationMapping<{email}, {short_name}>'.format(
             email=self.default_user.email,
@@ -76,67 +80,27 @@ class TestTahoeSiteUUID(DefaultsForTestsMixin):
     """
     @pytest.mark.skipif(settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
                         reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is off')
-    def test_get_organization_by_uuid_without_org(self):
-        """
-        Test get_organization_by_uuid helper when edx-organizations customization is off
-        """
-        default_site = TahoeSiteUUID.objects.get(organization=self.default_org)
-        assert TahoeSiteUUID.get_organization_by_uuid(default_site.site_uuid) == self.default_org
-
-    @pytest.mark.skipif(settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
-                        reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is off')
-    def test_get_uuid_by_organization_without_org(self):
-        """
-        Test get_uuid_by_organization helper when edx-organizations customization is off
-        """
-        default_site = TahoeSiteUUID.objects.get(organization=self.default_org)
-        assert TahoeSiteUUID.get_uuid_by_organization(self.default_org) == default_site.site_uuid
-
-    @pytest.mark.skipif(not settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
-                        reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is on')
-    def test_get_organization_by_uuid_with_org(self):
-        """
-        Test get_organization_by_uuid helper when edx-organizations customization is on
-        """
-        assert TahoeSiteUUID.get_organization_by_uuid(self.default_org.edx_uuid) == self.default_org
-
-    @pytest.mark.skipif(not settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
-                        reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is on')
-    def test_get_uuid_by_organization_with_org(self):
-        """
-        Test get_uuid_by_organization helper when edx-organizations customization is on
-        """
-        assert TahoeSiteUUID.get_uuid_by_organization(self.default_org) == self.default_org.edx_uuid
-
-    @staticmethod
-    @pytest.mark.skipif(settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
-                        reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is off')
-    def test_create_organization_signal():
+    def test_create_organization_signal(self):
         """
         Verify that creating an Organization object will signal a creation for TahoeUUISite
         """
         sites_count = TahoeSiteUUID.objects.count()
-        organization = Organization.objects.create(
+        organization = self._create_organization(
             name='dummy organization',
-            description='dummy organization description',
-            active=True,
             short_name='DO',
         )
         assert TahoeSiteUUID.objects.count() == sites_count + 1
         assert TahoeSiteUUID.objects.get(organization=organization)
 
-    @staticmethod
     @pytest.mark.skipif(settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
                         reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is off')
-    def test_save_organization_dose_not_create_site():
+    def test_save_organization_dose_not_create_site(self):
         """
         Verify that saving an Organization object will not create another TahoeUUISite
         """
         sites_count = TahoeSiteUUID.objects.count()
-        organization = Organization.objects.create(
+        organization = self._create_organization(
             name='dummy organization',
-            description='dummy organization description',
-            active=True,
             short_name='DO',
         )
         organization.name = 'new name'
