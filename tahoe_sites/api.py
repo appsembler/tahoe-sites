3cc6c41554435cfcabba4cc46604cf66d18373b6
Omar Al-Ithawi <i@omardo.com>
2022-02-07 12:41:51 +0300

    `helpers` --> `api` to codify backward compatibility and commitments

---
+++ tahoe_sites/api.py
@@ -0,0 +1,118 @@
+"""
+External Python API helpers goes here.
+
+Those APIs should be stable and abstract internal model changes.
+Non-stable APIs they should be placed in the `helpers.py` module instead.
+
+
+### API Contract:
+
+ * The parameters of existing functions should change in a backward compatible way:
+   - No parameters should be removed from the function
+   - New parameters should have safe defaults
+ * For breaking changes, new functions should be created
+"""
+
+from django.contrib.auth import get_user_model
+from organizations.models import Organization
+
+from tahoe_sites import zd_helpers
+from tahoe_sites.models import TahoeSiteUUID, UserOrganizationMapping
+
+
+def get_organization_by_uuid(organization_uuid):
+    """
+    Get an organization object from it's uuid
+
+    :param organization_uuid: uuid to filter on
+    :return: organization of the given uuid
+    """
+    if zd_helpers.should_site_use_org_models():
+        return Organization.objects.get(edx_uuid=organization_uuid)
+    return TahoeSiteUUID.objects.get(site_uuid=organization_uuid).organization
+
+
+def get_uuid_by_organization(organization):
+    """
+    Get the uuid when from it's related organization
+
+    :param organization: organization to filter on
+    :return: uuid of the given organization
+    """
+    if zd_helpers.should_site_use_org_models():
+        return organization.edx_uuid
+    return TahoeSiteUUID.objects.get(organization=organization).site_uuid
+
+
+def get_organizations_for_user(user, with_inactive_users=False, without_admins=False):
+    """
+    Return organizations related to the given user. By default, the list will ignore
+    organizations where the user is inactive
+
+    :param user: user to filter on
+    :param with_inactive_users: include organizations where the user is inactive (default = False)
+    :param without_admins: exclude organizations where the user is set as admin (default = False)
+    :return: Organization objects related to the given user
+    """
+    if with_inactive_users:
+        extra_params = {}
+    else:
+        extra_params = {'is_active': True}
+    if without_admins:
+        extra_params['is_admin'] = False
+
+    return Organization.objects.filter(
+        pk__in=UserOrganizationMapping.objects.filter(user=user, **extra_params).values('organization_id')
+    )
+
+
+def get_users_of_organization(organization, with_inactive_users=False, without_admins=False):
+    """
+    Return users related to the given organization. By default, all active users will be
+    returned including admin users
+
+    :param organization: organization to filter on
+    :param with_inactive_users: include inactive users in the result (default = False)
+    :param without_admins: exclude admin users from the result (default = False)
+    :return: User objects related to the given organization
+    """
+    if with_inactive_users:
+        extra_params = {}
+    else:
+        extra_params = {'is_active': True}
+    if without_admins:
+        extra_params['is_admin'] = False
+    return get_user_model().objects.filter(
+        pk__in=UserOrganizationMapping.objects.filter(organization=organization, **extra_params).values('user_id')
+    )
+
+
+def is_active_admin_on_any_organization(user, org_ids):
+    """
+    Check if the given user is an admin on any of the given organizations
+
+    :param user: user to filter on
+    :param org_ids: QuerySet of organization ids to filter on
+    :return: <True> if user is an admin on any organization, <False> otherwise
+    """
+    return UserOrganizationMapping.objects.filter(
+        user=user,
+        organization_id__in=org_ids,
+        is_active=True,
+        is_admin=True,
+    ).exists()
+
+
+def is_active_admin_on_organization(user, organization):
+    """
+    Check if the given user is an admin on the given organizations
+    :param user: user to filter on
+    :param organization:  organization to filter on
+    :return: <True> if user is an admin on the organization, <False> otherwise
+    """
+    return UserOrganizationMapping.objects.filter(
+        user=user,
+        organization=organization,
+        is_active=True,
+        is_admin=True,
+    ).exists()
--- tahoe_sites/tests/test_api.py
+++ tahoe_sites/tests/test_api.py
@@ -5,16 +5,16 @@ import pytest
 from django.conf import settings
 from organizations.models import Organization
 
-from tahoe_sites import helpers
+from tahoe_sites import api
 from tahoe_sites.models import TahoeSiteUUID
 from tahoe_sites.tests.fatories import UserFactory
 from tahoe_sites.tests.test_models import DefaultsForTestsMixin
 from tahoe_sites.tests.utils import create_organization_mapping
 
 
-class TestHelpers(DefaultsForTestsMixin):
+class TestAPIHelpers(DefaultsForTestsMixin):
     """
-    Tests for helpers
+    Tests for API helpers
     """
     def setUp(self):
         super().setUp()
@@ -53,7 +53,7 @@ class TestHelpers(DefaultsForTestsMixin):
         Test get_organization_by_uuid helper when edx-organizations customization is off
         """
         default_site = TahoeSiteUUID.objects.get(organization=self.default_org)
-        assert helpers.get_organization_by_uuid(default_site.site_uuid) == self.default_org
+        assert api.get_organization_by_uuid(default_site.site_uuid) == self.default_org
 
     @pytest.mark.skipif(settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
                         reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is off')
@@ -62,7 +62,7 @@ class TestHelpers(DefaultsForTestsMixin):
         Test get_uuid_by_organization helper when edx-organizations customization is off
         """
         default_site = TahoeSiteUUID.objects.get(organization=self.default_org)
-        assert helpers.get_uuid_by_organization(self.default_org) == default_site.site_uuid
+        assert api.get_uuid_by_organization(self.default_org) == default_site.site_uuid
 
     @pytest.mark.skipif(not settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
                         reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is on')
@@ -70,7 +70,7 @@ class TestHelpers(DefaultsForTestsMixin):
         """
         Test get_organization_by_uuid helper when edx-organizations customization is on
         """
-        assert helpers.get_organization_by_uuid(self.default_org.edx_uuid) == self.default_org
+        assert api.get_organization_by_uuid(self.default_org.edx_uuid) == self.default_org
 
     @pytest.mark.skipif(not settings.FEATURES['TAHOE_SITES_USE_ORGS_MODELS'],
                         reason='Runs only when TAHOE_SITES_USE_ORGS_MODELS is on')
@@ -78,7 +78,7 @@ class TestHelpers(DefaultsForTestsMixin):
         """
         Test get_uuid_by_organization helper when edx-organizations customization is on
         """
-        assert helpers.get_uuid_by_organization(self.default_org) == self.default_org.edx_uuid
+        assert api.get_uuid_by_organization(self.default_org) == self.default_org.edx_uuid
 
     def test_get_organizations_for_user_default(self):
         """
@@ -87,15 +87,15 @@ class TestHelpers(DefaultsForTestsMixin):
         self._prepare_mapping_data()
 
         # default_user is mapped to 2 of them
-        assert list(helpers.get_organizations_for_user(self.default_user)) == [self.default_org, self.org1]
+        assert list(api.get_organizations_for_user(self.default_user)) == [self.default_org, self.org1]
 
         # user2 is mapped to two of them, one shared with default_user
-        assert list(helpers.get_organizations_for_user(self.user2)) == [self.org1, self.org2]
+        assert list(api.get_organizations_for_user(self.user2)) == [self.org1, self.org2]
 
         # records with inactive user will not be returned
         self.mapping.is_active = False
         self.mapping.save()
-        assert list(helpers.get_organizations_for_user(self.default_user)) == [self.default_org]
+        assert list(api.get_organizations_for_user(self.default_user)) == [self.default_org]
 
     def test_get_organizations_for_user_with_inactive_users(self):
         """
@@ -106,7 +106,7 @@ class TestHelpers(DefaultsForTestsMixin):
 
         self.mapping.is_active = False
         self.mapping.save()
-        assert list(helpers.get_organizations_for_user(self.default_user, with_inactive_users=True)) == [
+        assert list(api.get_organizations_for_user(self.default_user, with_inactive_users=True)) == [
             self.default_org,
             self.org1
         ]
@@ -120,7 +120,7 @@ class TestHelpers(DefaultsForTestsMixin):
 
         self.mapping.is_admin = True
         self.mapping.save()
-        assert list(helpers.get_organizations_for_user(self.default_user, without_admins=True)) == [self.default_org]
+        assert list(api.get_organizations_for_user(self.default_user, without_admins=True)) == [self.default_org]
 
     def test_get_users_of_organization(self):
         """
@@ -129,15 +129,15 @@ class TestHelpers(DefaultsForTestsMixin):
         self._prepare_mapping_data()
 
         # default_org is mapped to default_user
-        assert list(helpers.get_users_of_organization(self.default_org)) == [self.default_user]
+        assert list(api.get_users_of_organization(self.default_org)) == [self.default_user]
 
         # Org1 is mapped to two users
-        assert list(helpers.get_users_of_organization(self.org1)) == [self.default_user, self.user2]
+        assert list(api.get_users_of_organization(self.org1)) == [self.default_user, self.user2]
 
         # inactive users will not be returned
         self.mapping.is_active = False
         self.mapping.save()
-        assert list(helpers.get_users_of_organization(self.org1)) == [self.user2]
+        assert list(api.get_users_of_organization(self.org1)) == [self.user2]
 
     def test_get_users_of_organization_with_inactive_users(self):
         """
@@ -148,7 +148,7 @@ class TestHelpers(DefaultsForTestsMixin):
 
         self.mapping.is_active = False
         self.mapping.save()
-        assert list(helpers.get_users_of_organization(self.org1, with_inactive_users=True)) == [
+        assert list(api.get_users_of_organization(self.org1, with_inactive_users=True)) == [
             self.default_user,
             self.user2
         ]
@@ -162,7 +162,7 @@ class TestHelpers(DefaultsForTestsMixin):
 
         self.mapping.is_admin = True
         self.mapping.save()
-        assert list(helpers.get_users_of_organization(self.org1, without_admins=True)) == [self.user2]
+        assert list(api.get_users_of_organization(self.org1, without_admins=True)) == [self.user2]
 
     def test_is_active_admin_on_organization(self):
         """
@@ -171,8 +171,8 @@ class TestHelpers(DefaultsForTestsMixin):
         """
         self._prepare_mapping_data()
 
-        assert not helpers.is_active_admin_on_organization(user=self.default_user, organization=self.org1)
+        assert not api.is_active_admin_on_organization(user=self.default_user, organization=self.org1)
 
         self.mapping.is_admin = True
         self.mapping.save()
-        assert helpers.is_active_admin_on_organization(user=self.default_user, organization=self.org1)
+        assert api.is_active_admin_on_organization(user=self.default_user, organization=self.org1)
